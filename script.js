52c9bd8472788120e16da11358ed6e3201200b2d
Kaptech <125941565+kaptech-dev@users.noreply.github.com>
2026-02-17 20:09:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Mendeteksi scroll untuk merubah tampilan Navbar
+const navbar = document.getElementById('navbar');
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('nav-scrolled');
+    } else {
+        navbar.classList.remove('nav-scrolled');
+    }
+});
+
+// Pesan sambutan di Console (Opsional)
+console.log("%c Kaptech Dev Portfolio v1.0 ", "background: #3b82f6; color: white; font-weight: bold;");
